aca593cc64b6890a22b987d5d4fffdce1a4faeb7
RupinderKaur14 <rupinderkaur9202@gmail.com>
2023-11-21 13:21:04 -0800

    add routing to house Listing and details Page

--- firstApp/src/app/app.component.ts
+++ firstApp/src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HomeComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, HomeComponent],
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
+ <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
   `,
