4bf5b153e07b0bc22905cc2ff4468e062d2c23cc
RupinderKaur14 <rupinderkaur9202@gmail.com>
2023-11-20 16:01:03 -0800

    created home component

--- firstApp/src/app/app.component.spec.ts
+++ firstApp/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'firstApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstApp');
+    expect(app.title).toEqual('Homes');
   });
 
   it('should render title', () => {
